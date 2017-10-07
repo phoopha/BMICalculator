bcbaa6ace53563a2bf770123286577ae8fb67423
Niphawan Phoopha <phoopha.miw@gmail.com>
2017-10-07 20:37:47 +0700

    amm

--- app/src/main/java/com/example/user/bmicalculator/BmiResultActivity.java
+++ app/src/main/java/com/example/user/bmicalculator/BmiResultActivity.java
@@ -12,11 +12,15 @@ public class BmiResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_result);
+
         t = (TextView) findViewById(R.id.text_view);
+
         Intent intent = getIntent();
         Double bmi = intent.getDoubleExtra("bmi_value", 0);
         String bmiText = intent.getStringExtra("bmi_text");
+
         String result = String.format("ค่า BMI ที่ได้คือ %.2f\n\nอยู่ในเกณฑ์ : %s", bmi, bmiText);
+
         t.setText(result);
     }
 }
